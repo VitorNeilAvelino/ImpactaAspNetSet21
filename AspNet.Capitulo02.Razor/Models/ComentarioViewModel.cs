eb7c99f88110f4f472fd014ee262dd038a0b2193
Vitor Neil Avelino <avelino.vitor@gmail.com>
2021-09-23 22:04:56 -0300

    Add project files.

---
+++ AspNet.Capitulo02.Razor/Models/ComentarioViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AspNet.Capitulo02.Razor.Models
+{
+    public class ComentarioViewModel
+    {
+        public DateTime Data { get; set; }
+        public string Comentarista { get; set; }
+        public string Conteudo { get; set; }
+    }
+}
