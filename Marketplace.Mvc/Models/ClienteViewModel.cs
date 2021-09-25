9785f6f5f124409ccc510d169155529cff72c559
Vitor Neil Avelino <avelino.vitor@gmail.com>
2021-09-24 22:59:35 -0300

    Inserção de Cliente

---
+++ Marketplace.Mvc/Models/ClienteViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Marketplace.Mvc.Models
+{
+    public class ClienteViewModel
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(14)]
+        public string Documento { get; set; }
+        
+        [Required]
+        public string Nome { get; set; }
+        
+        [Required]
+        public string Telefone { get; set; }
+        
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
