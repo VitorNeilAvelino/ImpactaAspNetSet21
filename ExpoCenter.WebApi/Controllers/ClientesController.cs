412e71adb5e74586d3bf13dae303e20f58ec3fca
Vitor Neil Avelino <avelino.vitor@gmail.com>
2022-06-18 15:14:31 -0300

    Limpeza de código

--- ExpoCenter.WebApi/Controllers/ClientesController.cs
+++ ExpoCenter.WebApi/Controllers/ClientesController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ExpoCenter.Dominio.Entidades;
@@ -21,14 +19,12 @@ namespace ExpoCenter.WebApi.Controllers
             _context = context;
         }
 
-        // GET: api/Clientes
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Cliente>>> GetClientes()
         {
             return await _context.Clientes.ToListAsync();
         }
 
-        // GET: api/Clientes/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Cliente>> GetCliente(int id)
         {
